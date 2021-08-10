6e6915bc96a4d2ddd5f841406f12f3148c963664
daviddongguo <david.dong.guo@gmail.com>
2021-08-10 13:02:00 -0400

    Active food

--- src/components/Food.vue
+++ src/components/Food.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="food">
+  <div id="food" :style="{ left: left, top: top }">
     <div></div>
     <div></div>
     <div></div>
@@ -8,7 +8,33 @@
 </template>
 
 <script>
-export default {};
+export default {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  data() {
+    return {
+      x: 120,
+      y: 120,
+    };
+  },
+  computed: {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    left() {
+      return `${this.x.toString()}px`;
+    },
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    top() {
+      return `${this.y.toString()}px`;
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  mounted() {
+    const rnd = () => Math.floor(Math.random() * 30) * 10;
+    setInterval(() => {
+      this.x = rnd();
+      this.y = rnd();
+    }, 1000);
+  },
+};
 </script>
 
 <style></style>
